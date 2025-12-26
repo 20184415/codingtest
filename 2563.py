367a8c73d8b1e22a98ac895658a45f289154ba26
양찬혁 <20184415@s.scnu.ac.kr>
2025-12-26 15:51:42 +0900

    Revert "지금까지 한 것들 추가"
    This reverts commit ff2233e2f7df182dde353bab5c7d8f2dc484a949.

--- 2563.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-board = [[0]*100 for _ in range(100)]
-for _ in range(n):
-    x, y = map(int, input().split())
-    for i in range(x, x+10):
-        for j in range(y, y+10):
-            board[i][j] = 1
-print(sum(map(sum, board)))
-
